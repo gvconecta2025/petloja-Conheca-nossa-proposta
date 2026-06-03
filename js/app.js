7c0db98e8ffa4611585a0b34c1df197086505133
gvconecta2025 <gvconecta2025@gmail.com>
2026-06-03 12:04:24 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
-
 function startJourney(){
- document.getElementById('progressWrap').classList.remove('hidden');
- document.getElementById('s1').classList.remove('hidden');
- document.getElementById('s1').scrollIntoView({behavior:'smooth'});
- update(1);
-}
-function showStep(n){
- document.getElementById('s'+n).classList.remove('hidden');
- document.getElementById('s'+n).scrollIntoView({behavior:'smooth'});
- update(n);
-}
-function update(n){
- document.getElementById('progressText').innerText=n+' de 5 etapas';
- document.getElementById('bar').style.width=(n*20)+'%';
+    document.getElementById('progressWrap').classList.remove('hidden');
+    document.getElementById('s1').classList.remove('hidden');
+    document.getElementById('s1').scrollIntoView({behavior:'smooth'});
+    update(1);
 }
 
+function showStep(n){
+    document.getElementById('s'+n).classList.remove('hidden');
+    document.getElementById('s'+n).scrollIntoView({behavior:'smooth'});
+    update(n);
+}
+
+function update(n){
+    document.getElementById('progressText').innerText = n + ' de 5 etapas';
+    document.getElementById('bar').style.width = (n * 20) + '%';
+}
+
+document.getElementById("fontToggle").addEventListener("click", function(){
+    document.body.classList.toggle("large");
+});
